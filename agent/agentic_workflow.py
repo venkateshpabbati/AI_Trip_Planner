2dded2c8821f28e4574fc4775f8ed448a8e6e0d7
sunnysavita10 <engsunny3365@gmail.com>
2025-06-27 22:14:49 +0530

    tool flow updated

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -3,20 +3,34 @@ from utils.model_loader import ModelLoader
 from prompt_library.prompt import SYSTEM_PROMPT
 from langgraph.graph import StateGraph, MessagesState, END, START
 from langgraph.prebuilt import ToolNode, tools_condition
-# from tools.weather_info_tool import WeatherInfoTool
-# from tools.place_search_tool import PlaceSearchTool
-# from tools.expense_calculator_tool import CalculatorTool
-# from tools.currency_conversion_tool import CurrencyConverterTool
+from tools.weather_info_tool import WeatherInfoTool
+from tools.place_search_tool import PlaceSearchTool
+from tools.expense_calculator_tool import CalculatorTool
+from tools.currency_conversion_tool import CurrencyConverterTool
 
 class GraphBuilder():
-    def __init__(self):
-        self.tools= [
-            # WeatherInfoTool(),
-            # PlaceSearchTool(),
-            # CalculatorTool(),
-            # CurrencyConverterTool()
-        ]
-        self.system_prompt=SYSTEM_PROMPT
+    def __init__(self,model_provider: str = "groq"):
+        self.model_loader = ModelLoader(model_provider=model_provider)
+        self.llm = self.model_loader.load_llm()
+        
+        self.tools = []
+        
+        self.weather_tools = WeatherInfoTool()
+        self.place_search_tools = PlaceSearchTool()
+        self.calculator_tools = CalculatorTool()
+        self.currency_converter_tools = CurrencyConverterTool()
+        
+        self.tools.extend([* self.weather_tools.weather_tool_list, 
+                           * self.place_search_tools.place_search_tool_list,
+                           * self.calculator_tools.calculator_tool_list,
+                           * self.currency_converter_tools.currency_converter_tool_list])
+        
+        self.llm_with_tools = self.llm.bind_tools(tools=self.tools)
+        
+        self.graph = None
+        
+        self.system_prompt = SYSTEM_PROMPT
+    
     
     def agent_function(self,state: MessagesState):
         """Main agent function"""
